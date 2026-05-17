b11784567f1b7674a9a1d1a57d9dc1d0f26eb542
Ryan-Zoch <ryan.k.zoch@gmail.com>
2026-05-17 15:49:19 -0700

    fix4

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request){
     const form = await request.formData();
     const file = form.get("file") as File;
-    const blob = await put(file.name, file, { access: "public" });
+    const uniqueName = `${Date.now()}-${file.name}`;
+    const blob = await put(uniqueName, file, { access: "public" });
     return NextResponse.json(blob);
 }
